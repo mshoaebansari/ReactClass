3cb5b576a7951f3e9569414eb6446d061a291e9a
Aadil23exe <jb504535@gmail.com>
2025-10-20 11:36:58 +0530

    Created First Website Layout Using React

---
+++ Aadil/1-react-project/src/component/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './footer.css'
+function Footer ()
+    {
+        return (
+            <div className="MyFooter">&copy; 2025 Aadil & Co.</div>
+        )
+    }
+    export default Footer;
---
+++ Aadil/1-react-project/src/component/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './header.css';
+function Header()
+{
+    return(
+        <h1 className="MyHeader"> This is a header</h1>
+    );
+}
+
+export default Header;
