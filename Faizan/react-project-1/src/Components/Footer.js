a1b4ae5695618d1738d28aa6bc299201d669e228
faizan <faizanmemonjb@gmail.com>
2025-10-23 10:41:33 +0530

    my new work

---
+++ Faizan/react-project-1/src/Components/Footer.js
@@ -0,0 +1,10 @@
+import react from "react";
+import MyFooter from'./Footer.css';
+function Footer() {
+  return (
+   <div className="MyFooter">
+        My Footer
+    </div>
+  );
+}
+export default Footer;
