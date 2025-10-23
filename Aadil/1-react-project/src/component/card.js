d0eda076adc5972fc7e64f89926f45b89c8d2811
Aadil23exe <jb504535@gmail.com>
2025-10-23 11:26:05 +0530

    Created Cards in Container

---
+++ Aadil/1-react-project/src/component/card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './card.css'
+function Card () 
+{
+    return (
+        <div className="MyCard">
+            <h1>This is a Card</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+    )
+}
+export default Card;
